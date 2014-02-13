aa7864ca1c67a4802c91850c97b52591445e2fa4
Håkon Bogen <hakon.bogen@shortcut.no>
2014-02-13 22:20:08 +0100

    adding model for achievements

---
+++ Gamify/AAAAChievement.h
@@ -0,0 +1,13 @@
+//
+//  AAAAChievement.h
+//  Gamify
+//
+//  Created by Håkon Bogen on 13.02.14.
+//  Copyright (c) 2014 Haaakon Bogen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AAAAChievement : NSObject
+
+@end
---
+++ Gamify/AAAAChievement.m
@@ -0,0 +1,13 @@
+//
+//  AAAAChievement.m
+//  Gamify
+//
+//  Created by Håkon Bogen on 13.02.14.
+//  Copyright (c) 2014 Haaakon Bogen. All rights reserved.
+//
+
+#import "AAAAChievement.h"
+
+@implementation AAAAChievement
+
+@end
