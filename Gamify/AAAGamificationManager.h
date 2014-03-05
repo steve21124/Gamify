770a0d31ec5fb1397b3bd8151c5ae93a11042296
Håkon Bogen <hakon.bogen@shortcut.no>
2014-03-05 07:23:09 +0100

    adding more documentation

--- Gamify/AAAGamificationManager.h
+++ Gamify/AAAGamificationManager.h
@@ -16,16 +16,37 @@
 + (AAAGamificationManager *)sharedManager;
 
 - (void)initialSetScoreNoAnimation;
+/**
+ *  Sets score as the main players current score, 
+ *  disregarding what the score was previously
+ *  If main players score is 15, setMainPlayersScore is called with 5
+ *  main players score should be 5 after method is run
+ *
+ *  @param score to set as main players score
+ */
 - (void)setMainPlayersScore:(NSInteger)score;
+/**
+ *  Adds the score to the main players current score. 
+ *  If main players score is 15, addToMainPlayersScore is called with 5
+ *  main players score should be 20 after method is run
+ *
+ *  @param score to add to existing players score
+ */
 - (void)addToMainPlayerScore:(NSInteger)score;
+/**
+ *  Current score of the main player in the app, this
+ *  score is automatically stored using nscoding.
+ *
+ *  @return the score of the player
+ */
 - (NSInteger)mainPlayerScore;
 
 - (void)showAchievementViewControllerOnViewController:(UIViewController*)viewController withAchievement:(AAAAchievement*)achievement;
 /**
  *  Add achievement to the controller
  *
- *  @param achievement <#achievement description#>
- *  @param key         <#key description#>
+ *  @param achievement 
+ *  @param key
  */
 - (void)addAchievement:(AAAAchievement*) achievement forKey:(NSString*)key;
 
