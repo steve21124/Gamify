7ac5fb249ab0eb8e9b2045a3ecec646c190abb85
Håkon Bogen <hakon.bogen@shortcut.no>
2014-03-05 07:21:48 +0100

    adding manager for achievements

---
+++ Gamify/AAAAchievementManager.h
@@ -0,0 +1,13 @@
+//
+//  AAAAchievementManager.h
+//  Gamify
+//
+//  Created by Håkon Bogen on 04.03.14.
+//  Copyright (c) 2014 Haaakon Bogen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AAAAchievementManager : NSObject
+
+@end
---
+++ Gamify/AAAAchievementManager.m
@@ -0,0 +1,13 @@
+//
+//  AAAAchievementManager.m
+//  Gamify
+//
+//  Created by Håkon Bogen on 04.03.14.
+//  Copyright (c) 2014 Haaakon Bogen. All rights reserved.
+//
+
+#import "AAAAchievementManager.h"
+
+@implementation AAAAchievementManager
+
+@end
